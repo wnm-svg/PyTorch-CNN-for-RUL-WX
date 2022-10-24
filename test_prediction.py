e8fc55786b1cb6f123071456b51b162087358554
wnm-svg <2589785188@qq.com>
2022-10-24 09:28:24 +0800

    修改test_prediction

--- test_prediction.py
+++ test_prediction.py
@@ -8,12 +8,12 @@ def test_prediction(model, test_x):
         output = model(test_x)
 
 
-    softmax = torch.exp(output).cpu()
-    # print(softmax[0:1])
-    prob = list(softmax.numpy())
-    # print(prob[0:1])
-    # index of max value 
-    predictions = np.argmax(prob, axis=1)
+    # softmax = torch.exp(output).cpu()
+    # # print(softmax[0:1])
+    # prob = list(softmax.numpy())
+    # # print(prob[0:1])
+    # # index of max value 
+    # predictions = np.argmax(prob, axis=1)
     # print(predictions[0:1])
 
-    return predictions
+    return output
